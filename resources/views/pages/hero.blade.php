a288a6fa614c22c3be06f2e2096e3013372c1e56
Mehedi Hasan Ahad <mehedihasanahad07@gmail.com>
2023-05-08 23:36:35 +0600

    footer & hero page first component done

--- resources/views/pages/hero.blade.php
+++ resources/views/pages/hero.blade.php
@@ -1,5 +1,15 @@
 @extends('layout.layout')
 @section('title', 'Hero Page')
 @section('pages')
-<div class="h-[1500px]">Hero page</div>
+    <div class="my-2 sm:flex justify-between h-[330px] sm:h-[350px] lg:h-[600px] items-center">
+        <h1 class="hidden sm:block text-3xl w-1/2 font-bold text-fuchsia-700">
+            THE BEST <span class="text-pink-400">EDUCATION</span> THEME
+        </h1>
+        <div class="header-svg
+        top-[90px] right-[15%] bottom-0 left-[15%]
+        sm:top-[108px] sm:right-[4%] sm:left-1/2
+        lg:top-[148px] lg:right-[8%] lg:left-1/2">
+            @includeIf('subviews.hero-svg')
+        </div>
+    </div>
 @endsection
--- resources/views/subviews/layout/footer.blade.php
+++ resources/views/subviews/layout/footer.blade.php
@@ -1,5 +1,44 @@
-<div class="lg:w-[1200px] lg:m-auto px-2 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
-<div>1</div>
-<div>2</div>
-<div>3</div>
+<div class="lg:w-[1200px] lg:m-auto px-2">
+    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-x-2.5 pt-16 pb-12">
+        <div>
+            <h1 class="font-bold text-xl">Social</h1>
+            <br/>
+            <a href="javascript:void(0)" class="flex">
+                <div class="p-2 bg-sky-200 rounded-full mb-2">
+                    <img class="w-4" src="{{asset('assets/static/images/facebook.svg')}}"/></div>
+                <p class="ml-2">Facebook</p>
+            </a>
+            <a href="javascript:void(0)" class="flex">
+                <div class="p-2 bg-sky-200 rounded-full">
+                    <img class="w-4" src="{{asset('assets/static/images/twitter.svg')}}"/>
+                </div>
+                <p class="ml-2">Twitter</p>
+            </a>
+        </div>
+        <div>
+            <h1 class="font-bold text-xl">Latest posts</h1>
+            <br/>
+            @foreach([1,2,3] as $index=>$value)
+                <article class="flex gap-x-1 mb-6">
+                    <img class="w-20 h-20 object-cover rounded-md" src="{{asset('assets/static/images/dummy.jpeg')}}">
+                    <h3 class="ml-2">Autumn is a second spring when every leaf is a flower</h3>
+                </article>
+            @endforeach
+        </div>
+        <div>
+            <h1 class="font-bold text-xl">Latest posts</h1>
+            <br/>
+            <div class="flex gap-2 flex-wrap">
+                @foreach([1,2,3,4,5] as $index=>$value)
+                    <div class="flex items-center bg-slate-200 py-1 px-3.5 rounded-[20px]">
+                        <div class="w-2 h-2 rounded-full mr-2 bg-violet-800"></div>
+                        <p class="text-sm font-thin">Food</p>
+                    </div>
+                @endforeach
+            </div>
+        </div>
+    </div>
+    <p class="pb-8">
+        &copy; 2023 Neon - All right Reserved. Proudly Published with Ghost
+    </p>
 </div>
